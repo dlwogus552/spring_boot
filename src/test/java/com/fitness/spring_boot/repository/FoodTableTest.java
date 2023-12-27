7b74578fd628a81127ad18efe7e9d5e2de14c737
Bookmarked-Cat <gprufdudn123@naver.com>
2023-12-27 16:38:57 +0900

    .

---
+++ src/test/java/com/fitness/spring_boot/repository/FoodTableTest.java
@@ -0,0 +1,18 @@
+package com.fitness.spring_boot.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class FoodTableTest {
+    @Autowired
+    private FoodTableRepository repository;
+
+    @Test
+    public void select1() {
+        log.info(" df");
+    }
+}
