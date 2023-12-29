41563d0a71fd580f81f471e68e4a54842c34fd3f
Bookmarked-Cat <gprufdudn123@naver.com>
2023-12-29 12:50:17 +0900

    foodInfo 수정

--- src/main/java/com/fitness/spring_boot/controller/foodInfoController.java
+++ src/main/java/com/fitness/spring_boot/controller/foodInfoController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -20,7 +21,13 @@ public class foodInfoController {
     private final FoodInfoServiceImpl service;
 
     @GetMapping("/info")
-    public void searchList(String keyword, Model model) {
+    public void view() {
+
+    }
+
+    @GetMapping("/info/{keyword}")
+    @ResponseBody
+    public void searchList(@PathVariable("keyword") String keyword, Model model) {
         List<FoodInfoDTO> dtoList = service.search(keyword);
         log.info(dtoList);
         model.addAttribute("list", dtoList);
