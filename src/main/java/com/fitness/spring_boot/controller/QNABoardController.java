e522f7e106d83183e68ab426b460a331c040383b
Bookmarked-Cat <gprufdudn123@naver.com>
2024-01-03 16:43:35 +0900

    qna 디자인 구현중

--- src/main/java/com/fitness/spring_boot/controller/QNABoardController.java
+++ src/main/java/com/fitness/spring_boot/controller/QNABoardController.java
@@ -1,4 +1,43 @@
 package com.fitness.spring_boot.controller;
 
+import com.fitness.spring_boot.Service.qna.QNABoardService;
+import com.fitness.spring_boot.dto.PageRequestDTO;
+import com.fitness.spring_boot.dto.PageResponseDTO;
+import com.fitness.spring_boot.dto.qna.QNABoardDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/QnA")
+@Log4j2
+@Controller
+@RequiredArgsConstructor
 public class QNABoardController {
+    private final QNABoardService service;
+
+    @GetMapping("/register")
+    public void QnABoardWrite() {
+
+    }
+    @PostMapping("/register")
+    public String QnABoardWrite(QNABoardDTO qnaBoardDTO) {
+        Long qnabno = service.register(qnaBoardDTO);
+        if(qnabno != null) {
+            log.info("board 작성 됨 : "+qnabno);
+            return "redirect:/QnA/list";
+        }
+        return "redirect:/QnA/write";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+        PageResponseDTO<QNABoardDTO> responseDTO = service.getList(pageRequestDTO);
+        log.info(pageRequestDTO);
+        log.info(responseDTO);
+        model.addAttribute("responseDTO", responseDTO);
+    }
 }
