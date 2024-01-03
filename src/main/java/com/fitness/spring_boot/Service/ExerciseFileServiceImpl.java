41c24429abe9ccb18f70de2490d7d44b0aa78aee
dlwogus552 <145645760+dlwogus552@users.noreply.github.com>
2024-01-03 10:56:02 +0900

    Exercise(24.01.03) List 완성

--- src/main/java/com/fitness/spring_boot/Service/ExerciseFileServiceImpl.java
+++ src/main/java/com/fitness/spring_boot/Service/ExerciseFileServiceImpl.java
@@ -1,20 +1,32 @@
 package com.fitness.spring_boot.Service;
 
 import com.fitness.spring_boot.domain.ExerciseFile;
+import com.fitness.spring_boot.dto.ExerciseDTO;
 import com.fitness.spring_boot.dto.ExerciseFileDTO;
 import com.fitness.spring_boot.repository.ExerciseFileUploadRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class ExerciseFileServiceImpl implements ExerciseFileService{
     private final ExerciseFileUploadRepository exerciseFileUploadRepository;
+    private final ModelMapper modelMapper;
 
     @Override
-    public List<ExerciseFileDTO> getList(Long eno) {
-        return null;
+    public List<ExerciseFileDTO> getList() {
+        List<ExerciseFile> result = exerciseFileUploadRepository.findAll();
+        List<ExerciseFileDTO> list = result.stream().map(files -> modelMapper.map(files, ExerciseFileDTO.class)).collect(Collectors.toList());
+        return list;
+    }
+    @Override
+    public List<ExerciseFileDTO> getViewList(Long eno) {
+        List<ExerciseFileDTO> result = exerciseFileUploadRepository.findByExercise_EnoOrderByEfno(eno);
+        return result;
     }
 
     @Override
